393cf19878eb3fd6ca38dff88a4b0b2cad2f30e8
ZackDelsnova <96685170+ZackDelsnova@users.noreply.github.com>
2025-10-15 23:22:54 +0530

    initial board and print somewhat board

--- main.c
+++ main.c
@@ -9,8 +9,39 @@ enter [row] [column]
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define SIZE 3
+
+// Board 'X', 'O' OR '_'
+char board[SIZE][SIZE];
+
+// initialize board
+void initBoard() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            board[i][j] = '_';
+        }
+    }
+}
+
+// display board
+void printBoard() {
+    puts("\n  1 2 3");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", i+1);
+        for (int j = 0; j < SIZE; j++) {
+            printf("%c", board[i][j]);
+            if (j < SIZE - 1) putchar(' ');
+        }
+        putchar('\n');
+    }
+    putchar('\n');
+}
 
 int main() {
-    printf("hello world!");
+    initBoard();
+    printBoard();
     return 0;
 }
